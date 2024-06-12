a31843675e6d6550bf7c407ac6d3a7601904f509
aniya <24aniya_io@axiz.co.jp>
2024-06-12 15:44:42 +0900

    初期化済み

---
+++ src/main/java/com/makepcchecker/makepcchecker/MakePcCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.makepcchecker.makepcchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakePcCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakePcCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makepcchecker/makepcchecker/MakePcCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makepcchecker.makepcchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakePcCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
